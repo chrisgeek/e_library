2d8729aea1211a8672e4f9739f7527fb409b865f
chrisgeek <chrisgeek29@gmail.com>
2015-12-28 12:16:07 +0100

    changed email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => "587",
     :domain => "mail.google.com",
-    :user_name => "hillarynnamdievans@gmail.com",
-    :password => "hillarynnamdi",
+    :user_name => "chrisgeek29@gmail.com",
+    :password => "ifeanyi29",
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
