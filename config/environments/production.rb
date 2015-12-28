1bc2ced660e42ee059855beb148e3665cbc6a1e2
chrisgeek <chrisgeek29@gmail.com>
2015-12-28 02:23:53 +0100

    username and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,8 +90,8 @@ Rails.application.configure do
   :address        => "smtp.sendgrid.net",
   :port           => "587",
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'chrisgeek',
+  :password       => 'ifeanyi29',
   :domain         => "heroku.com",
   :enable_starttls_auto => true
 }
